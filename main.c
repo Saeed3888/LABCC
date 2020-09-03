7f905ace3e605ca50eb197d159485deb3289ebde
Saeed5062 <Ssa5468@psu.edu>
2020-09-03 13:57:22 +0000

    00

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+//Author: Saeed Alyami, ssa5468@psu,edu
+//Collaborator: Lucas Yancey, lcy5032@psu.edu
+//collaborator: Vincent Barnes, vjb5182@psu.edu
 
 int main(void) {
-  printf("Hello World\n");
+  char *temp = readline("Enter temperature in celsius: ");
+  double celsius;
+  celsius = atof(temp);
+  double fahrenheit;
+  fahrenheit = celsius* 1.8+32;
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n", celsius, fahrenheit);
   return 0;
 }
