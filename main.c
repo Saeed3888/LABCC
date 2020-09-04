9a079c013e1bf38954d5ea1650ff5dc583f77333
Saeed5062 <Ssa5468@psu.edu>
2020-09-04 22:08:51 +0000

    00

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-#include <readline/readline.h>
-#include <stdlib.h>
-
 //Author: Saeed Alyami, ssa5468@psu,edu
 //Collaborator: Lucas Yancey, lcy5032@psu.edu
 //collaborator: Vincent Barnes, vjb5182@psu.edu
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
 
 int main(void) {
   char *temp = readline("Enter temperature in celsius: ");
