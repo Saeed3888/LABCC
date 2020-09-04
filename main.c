04d4ae0a4b281b3f6d5d2f2e21d2715f96ca6582
Saeed5062 <Ssa5468@psu.edu>
2020-09-04 22:15:48 +0000

    9

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-// Author: Saeed Alyami, ssa5468@psu,edu
-// Collaborator: Lucas Yancey, lcy5032@psu.edu
-// collaborator: Vincent Barnes, vjb5182@psu.edu
+// Author: Saeed Alyami ssa5468@psu,edu
+// Collaborator: Lucas Yancey lcy5032@psu.edu
+// collaborator: Vincent Barnes vjb5182@psu.edu
 
 #include <stdio.h>
 #include <readline/readline.h>
